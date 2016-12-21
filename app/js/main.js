de87ea25b20fcc107c2238345cfb23f8c77ccdee
Vladimir Dmitriev <defurator@mail.ru>
2016-12-21 20:16:29 +0200

    uspeh + services + how + team + half siteform

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 
+	$('#how_slider').bxSlider({
+	  //mode: 'fade',
+	  //captions: true
+	  auto: true,
+	  pager: false,
+	  minSlides: 4,
+	  maxSlides: 4,
+	  moveSlides: 1,
+	  slideWidth: 253,
+	  slideMargin: 28
+	});
 
 });
